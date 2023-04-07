ae9d29a7463b3b7e23c8feb3a6f41f5fb8ebe9e5
inspecthers <kg.geku@gmail.com>
2023-04-07 01:05:40 +0100

    Banner

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ function App() {
     <div className="flex flex-col min-h-screen justify-between bg-gray-100">
       <Header />
       <main className="mb-auto px-40 ">
-        <section className="flex bg-green-400 mt-10 flex-col justify-center items-center py-20">
-          <h1 className="text-6xl font-bold">
-            We invest in the world’s potential
+        <section className="flex    rounded-md shadow-xs mt-10 flex-col justify-center items-center py-20">
+          <h1 className="text-6xl font-bold text-gray-700">
+            Actualités accessibles avec narration
           </h1>
-          <p className="text-xl font-light py-2 text-center w-[700px]">
-            Here at Flowbite we focus on markets where technology, innovation,
-            and capital can unlock long-term value aalue aalue and drive
-            economic growth.
+          <p className="text-xl font-light py-2 text-gray-500 text-center w-[700px]">
+            Lorsque l'on visite le blog Narration, on plonge dans un univers de
+            découvertes et de partages inépuisables. Chaque jour, ce blog
+            propose des articles passionnants portant sur des thèmes variés
           </p>
         </section>
         <ArticleList />
